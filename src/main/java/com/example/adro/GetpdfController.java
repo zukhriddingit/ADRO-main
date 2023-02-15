69d983611e89c6f990ace04ae1e7069dbf3443da
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2023-02-15 14:43:11 +0500

    feat: pdf generator

---
+++ src/main/java/com/example/adro/GetpdfController.java
@@ -0,0 +1,37 @@
+package com.example.adro;
+
+import com.itextpdf.text.pdf.Barcode39;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.Random;
+
+public class GetpdfController {
+
+    @FXML
+    private Button downloadPdf;
+
+    Random random = new Random();
+
+    @FXML
+    void downloadPdf(ActionEvent event) {
+        String[] columnTitles = {"Movie", "Number of Tickets", "Total Price"};
+        String[][] data = {
+                {"Avengers: Endgame", "2", "30.00"},
+                {"Black Panther", "3", "45.00"},
+                {"Jurassic World: Dominion", "1", "15.00"},
+                {"The Lion King", "4", "60.00"}
+
+
+        };
+
+        //barcode
+        String barcode = String.valueOf(random.nextFloat()*10000);
+        Barcode39 barcod = new Barcode39();
+        barcod.setCode(barcode);
+
+//        PdfGenerator pdfGenerator = new PdfGenerator();
+        PdfGenerator.generatePdf("Ticket Reservation Receipt", columnTitles, data);
+    }
+}
